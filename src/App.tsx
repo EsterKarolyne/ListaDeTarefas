f71a40d04f9c9d5fb24ef982bd1cfbf44ffec472
unknown <esterknappverly@gmail.com>
2021-11-21 18:03:57 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react';
+import * as C from './App.styles';
+import {Item} from './types/Item';
+import {ListItem} from './components/ListItem';
+import {AddArea} from './components/Components/AddArea';
+
 
 function App() {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Correr', done: false },
+    { id: 2, name: 'Nadar', done: false },
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false,
+    });
+    setList(newList);
+  };
+
+  const handleTaskChange = (id:number, done:boolean) =>{
+    let newList = [...list]
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+    console.log(newList)
+
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>
+          Lista de Tarefas
+        </C.Header>
+
+        <AddArea onEnter={handleAddTask} />
+
+        {list.map((item, index) => (
+          <ListItem  onChange={handleTaskChange} key={index} item={item}></ListItem>
+        ))}
+
+      </C.Area>
+     
+    </C.Container>
   );
 }
 
